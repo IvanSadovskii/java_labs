709e842148648a8ec7293a3b6b92c25bffc345d4
m1rai <finsop@bk.ru>
2023-06-12 00:52:51 +0700

    Сделал ввод с файла и консоли

--- lab1/main/program/Main.java
+++ lab1/main/program/Main.java
@@ -6,16 +6,48 @@ import main.program.Factory.ICommand;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 
         Data data = new Data();
         FactoryCommand factory = new FactoryCommand();
-        ICommand cmd = factory.setCommand("pop");
-        data.push_stack(1.1);
-        data.push_stack(2.1);
-        cmd.doCommand(null, data);
-        cmd = factory.setCommand("print");
-        cmd.doCommand(null, data);
+        String[] input;
+
+        try {
+            File file = new File("C:\\Users\\black\\Desktop\\in.txt");
+            // обработать
+            Scanner scanner = new Scanner(file);
+            //обработать
+            while (scanner.hasNextLine()) {
+
+                input = scanner.nextLine().split(" "); //убрать магию + обработать
+
+                ICommand cmd = factory.setCommand(input[0]); // магия
+                cmd.doCommand(input, data);
+
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Ошибка при открытии файла, введите команды в консоль (для выхода exit)");
+            Scanner console = new Scanner(System.in);
+            String line_input;
+
+            while (!(line_input = console.nextLine()).equals("exit")){
+                //обработка
+                input = line_input.split(" ");
+                ICommand cmd = factory.setCommand(input[0]); // магия
+                cmd.doCommand(input, data);
+
+            }
+
+
+        }
+
     }
 }
